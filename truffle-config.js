0551881e597338c5b406fa6132b8cca281211d01
Ayrton Trujillo <ayrtonx123@gmail.com>
2019-07-31 19:48:55 -0400

    verify contract

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+var HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraURL = 'https://rinkeby.infura.io/v3/b61830d82fb14c989989844403db2a5b';
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -46,6 +46,12 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 4,            // Rinkeby's network id
+      gas: 5500000,
     },
 
     // Another network with more advanced options...
